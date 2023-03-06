54cedf6d21c8839695c70f2afe53a1cedc6344f9
EOS <wisedom2@gmail.com>
2023-03-06 18:00:17 +0900

    ㅂㄷㅈㅂㅈㄷㅈㅂㄷㅂㅈㄷㅂㅈㄷ

--- src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -1,19 +1,19 @@
 package jpabook.jpashop.domain.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+//@RunWith(SpringRunner.class) Junit4
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    //@Rollback(value = false)
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long savedId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test
+    //@Test(expected = IllegalStateException.class) Junit4
+    public void 중복_회원_가입() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        //Assertions.fail("예외가 발생해야한다.");
+    }
+}
